0c67b0743332b17d170addfdebd15dd6ae992fb0
AdrienGentizon <adrien.gentizon@gmail.com>
2021-02-21 20:37:19 +0100

    design: section Presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HerroBanner from './Components/HeroBanner';
+import Presentation from './Components/Presentation';
 import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
       <HerroBanner />
+      <Presentation />
     </div>
   );
 }
---
+++ src/Components/Presentation.tsx
@@ -0,0 +1,18 @@
+import qoveryPresentation from './../assets/svg/qovery-presentation.svg';
+
+export default function Presentation() {
+  return (
+    <div className="Presentation">
+      <div className="container">
+        <h2>How does it work ?</h2>
+        <p>
+          Qovery Engine is an open-source abstraction layer product that turns
+          easy apps deployment on AWS, GCP, Azure and others Cloud providers.
+          The Qovery Engine is coded in Rust and takes advantage of Terraform,
+          Helm, Kubectl, Docker to manage resources.
+        </p>
+        <img src={qoveryPresentation} alt="" />
+      </div>
+    </div>
+  );
+}
